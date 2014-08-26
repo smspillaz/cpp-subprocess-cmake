d028cfba2ea18d4b4f7258b4507d1b575d2f2302
Sam Spilsbury <smspillaz@gmail.com>
2014-08-26 20:14:02 +0800

    Move to imported-project-utils

--- FindCPPSubprocess.cmake
+++ FindCPPSubprocess.cmake
@@ -22,96 +22,20 @@
 #
 #    If this variable is not set, an error will be raised.
 
-macro (_import_library_from_extproject library_target location extproj)
-
-    add_library (${library_target} STATIC IMPORTED GLOBAL)
-    set_target_properties (${library_target}
-                           PROPERTIES IMPORTED_LOCATION ${location})
-    add_dependencies (${library_target} ${extproj})
-
-endmacro (_import_library_from_extproject)
-
-# Workaround for some generators setting a different output directory
-function (_get_build_directory_suffix_for_generator SUFFIX)
-
-    if (${CMAKE_GENERATOR} STREQUAL "Xcode")
-
-        if (CMAKE_BUILD_TYPE)
-
-            set (${SUFFIX} ${CMAKE_BUILD_TYPE} PARENT_SCOPE)
-
-        else (CMAKE_BUILD_TYPE)
-
-            set (${SUFFIX} "Debug" PARENT_SCOPE)
-
-        endif (CMAKE_BUILD_TYPE)
-
-    endif (${CMAKE_GENERATOR} STREQUAL "Xcode")
-
-endfunction (_get_build_directory_suffix_for_generator)
-
-macro (_append_cache_definition CACHE_OPTION VALUE CACHE_ARGUMENT_LINE)
-
-    if (DEFINED ${VALUE})
-        list (APPEND ${CACHE_ARGUMENT_LINE}
-              "-D${CACHE_OPTION}:string=${${VALUE}}")
-    endif (DEFINED ${VALUE})
-
-endmacro (_append_cache_definition)
-
-macro (_append_cache_library_path CACHE_OPTION LIBRARY CACHE_ARGUMENT_LINE)
-
-    if (DEFINED ${LIBRARY})
-
-        set (_location)
-        if (TARGET ${${LIBRARY}})
-            get_property (_location TARGET ${${LIBRARY}} PROPERTY LOCATION)
-        endif (TARGET ${${LIBRARY}})
-
-        # If a location is set, we should use that one, otherwise
-        # just pass the linker line of the library.
-        if (_location)
-            list (APPEND ${CACHE_OPTION}
-                  "-D${CACHE_ARGUMENT_LINE}:string=${_location}")
-        else (_location)
-            list (APPEND ${CACHE_OPTION}
-                  "-D${CACHE_ARGUMENT_LINE}:string=${${LIBRARY}}")
-        endif (_location)
-
-    endif (DEFINED ${LIBRARY})
-
-endmacro (_append_cache_library_path)
-
-macro (_append_dependencies LIBRARY DEPENDENCIES)
-
-    if (TARGET ${${LIBRARY}})
-
-        list (APPEND ${DEPENDENCIES} ${${LIBRARY}})
-
-    endif (TARGET ${${LIBRARY}})
-
-endmacro (_append_dependencies)
-
-macro (_append_extproject_variables LIBRARY
-                                    CACHE_ARGUMENT_LINE
-                                    DEPENDENCIES
-                                    CACHE_OPTION)
-
-    _append_cache_library_path (${CACHE_OPTION}
-                                ${LIBRARY}
-                                ${CACHE_ARGUMENT_LINE})
-    _append_dependencies (${LIBRARY} ${DEPENDENCIES})
-
-endmacro (_append_extproject_variables)
+include (imported-project-utils/ImportedProjectUtils)
 
 if (CPP_SUBPROCESS_EXTERNAL_SET_INCLUDE_DIRS AND
-    CPP_SUBPROCESS_EXTERNAL_SET_LIBRARY_DIRS AND
     CPP_SUBPROCESS_EXTERNAL_SET_LIBRARY)
+
+    set (CPP_SUBPROCESS_INCLUDE_DIR ${CPP_SUBPROCESS_EXTERNAL_SET_INCLUDE_DIR})
+    set (CPP_SUBPROCESS_LIBRARY polysquare_cpp_subprocess)
+
+    polysquare_import_utils_import_library (${CPP_SUBPROCESS_LIBRARY} STATIC
+                                            ${CPP_SUBPROCESS_EXTERNAL_SET_LIBRARY})
 
     set (CPP_SUBPROCESS_FOUND 1)
 
 endif (CPP_SUBPROCESS_EXTERNAL_SET_INCLUDE_DIRS AND
-       CPP_SUBPROCESS_EXTERNAL_SET_LIBRARY_DIRS AND
        CPP_SUBPROCESS_EXTERNAL_SET_LIBRARY)
 
 if (NOT CPP_SUBPROCESS_FOUND)
@@ -138,57 +62,37 @@ if (NOT CPP_SUBPROCESS_FOUND)
     set (EXTPROJECT_TARGET CPPSubprocess)
 
     # Pass definitions for other external libraries if they were already defined
-    set (CPP_SUBPROCESS_CACHE_DEFINITIONS)
-    set (CPP_SUBPROCESS_PROJECT_DEPENDENCIES)
-    _append_cache_definition (GTEST_EXTERNAL_SET_INCLUDE_DIR
-                              GTEST_INCLUDE_DIR
-                              CPP_SUBPROCESS_CACHE_DEFINITIONS)
-    _append_cache_definition (GMOCK_EXTERNAL_SET_INCLUDE_DIR
-                              GMOCK_INCLUDE_DIR
-                              CPP_SUBPROCESS_CACHE_DEFINITIONS)
-
-    _append_extproject_variables (GTEST_LIBRARY
-                                  GTEST_EXTERNAL_SET_LIBRARY
-                                  CPP_SUBPROCESS_PROJECT_DEPENDENCIES
-                                  CPP_SUBPROCESS_CACHE_DEFINITIONS)
-    _append_extproject_variables (GMOCK_LIBRARY
-                                  GMOCK_EXTERNAL_SET_LIBRARY
-                                  CPP_SUBPROCESS_PROJECT_DEPENDENCIES
-                                  CPP_SUBPROCESS_CACHE_DEFINITIONS)
-    _append_extproject_variables (GTEST_MAIN_LIBRARY
-                                  GTEST_EXTERNAL_SET_MAIN_LIBRARY
-                                  CPP_SUBPROCESS_PROJECT_DEPENDENCIES
-                                  CPP_SUBPROCESS_CACHE_DEFINITIONS)
-    _append_extproject_variables (GMOCK_MAIN_LIBRARY
-                                  GMOCK_EXTERNAL_SET_MAIN_LIBRARY
-                                  CPP_SUBPROCESS_PROJECT_DEPENDENCIES
-                                  CPP_SUBPROCESS_CACHE_DEFINITIONS)
+    # That means the definitions for Google Mock.
+    set (CACHE_DEFINITIONS)
+    set (PROJECT_DEPENDENCIES)
+    google_mock_get_cache_lines_and_deps_from_found (CACHE_DEFINITIONS
+                                                     PROJECT_DEPENDENCIES)
 
     ExternalProject_Add (${EXTPROJECT_TARGET}
                          URL ${CPP_SUBPROCESS_SOURCES_DIRECTORY}
                          PREFIX ${CPP_SUBPROCESS_PREFIX}
-                         CMAKE_CACHE_ARGS ${CPP_SUBPROCESS_CACHE_DEFINITIONS}
+                         CMAKE_CACHE_ARGS ${CACHE_DEFINITIONS}
                          INSTALL_COMMAND "")
 
     add_dependencies (${EXTPROJECT_TARGET}
-                      ${CPP_SUBPROCESS_PROJECT_DEPENDENCIES})
+                      ${PROJECT_DEPENDENCIES})
 
     set (CPP_SUBPROCESS_LIBRARY polysquare_cpp_subprocess)
 
-    set (BINARY_DIR_SUFFIX)
-
-    _get_build_directory_suffix_for_generator (BINARY_DIR_SUFFIX)
+    set (SUFFIX)
+    polysquare_import_utils_get_build_suffix_for_generator (SUFFIX)
 
     set (_library_name libpolysquare_cpp_subprocess.a)
     set (CPP_SUBPROCESS_LIBRARY_PATH
-         ${CPP_SUBPROCESS_OUTPUT_BINARY_DIR}/${BINARY_DIR_SUFFIX}/${_library_name})
+         ${CPP_SUBPROCESS_OUTPUT_BINARY_DIR}/${SUFFIX}/${_library_name})
 
     set (CPP_SUBPROCESS_INCLUDE_DIR ${CPP_SUBPROCESS_SOURCE_DIR}/include)
 
     # Tell CMake that we've imported some libraries
-    _import_library_from_extproject (${CPP_SUBPROCESS_LIBRARY}
-                                     ${CPP_SUBPROCESS_LIBRARY_PATH}
-                                     ${EXTPROJECT_TARGET})
+    polysquare_import_utils_library_from_extproject (${CPP_SUBPROCESS_LIBRARY}
+                                                     STATIC
+                                                     ${CPP_SUBPROCESS_LIBRARY_PATH}
+                                                     ${EXTPROJECT_TARGET})
 
     set (CPP_SUBPROCESS_FOUND 1)
 
